5d8500f980aa37d0fd44aa5c29d301d2b45c69db
hamza01Al <hamza.mahboob01@gmail.com>
2025-12-28 19:35:52 +0500

    commit

--- map.cpp
+++ map.cpp
@@ -1,88 +1,89 @@
 #include <iostream>
 #include <random>
+#include <chrono>
 #include <vector>
-
 #include <string>
 using namespace std;
 
-
-
-int getRandomInt(int min, int max) {
-    static random_device rd;
-    static mt19937 gen(rd());
-    uniform_int_distribution<> dist(min, max);
+int westrandom(int min, int max) {
+    static mt19937 gen(chrono::steady_clock::now().time_since_epoch().count());
+    uniform_int_distribution<int> dist(min, max);
     return dist(gen);
 }
- 
-void westRoad(char road[5][15]) {
-    
 
-    // initialize with spaces
-    for (int i = 0; i < road[i]; i++)
-        for (int j = 0; j < cols; j++)
-            road[i][j] = ' ';
-
-    // fill rows 0 and 4 with consecutive *'s
-    for (int j = 0; j < road[j]; j++) {
-        road[0][j] = '*';
-        road[4][j] = '*';
-    }
-
-    // fill row 2 with consecutive -'s
-    for (int j = 0; j < road[j]; j++) {
-        road[2][j] = '-';
-    }
-
-    // print the road
-    for (int i = 0; i < road[i]; i++) {
-        for (int j = 0; j < road[j]; j++) {
-            cout << road[i][j];
-        }
-        cout << endl;
-    }
-    
-}
-
-struct car
-{   char represent='c';
+struct car {
+    char represent = 'c';
     int col;
     int row;
     string position;
-
 };
 
+struct WestJunction {
+    int noofcars = 0;
+    char signal = 'R';
+};
 
-void startwestcar(road,&car,vector &westcar)
-{
-  car temp;
-  road[2][5]=temp.represent;
-  temp.position="West";
-  push_back(westcar);
+struct EastJunction {
+    int noofcars = 0;
+    char signal = 'R';
+};
+
+void buildWestRoad(char road[5][15]) {
+    for (int i = 0; i < 5; i++)
+        for (int j = 0; j < 15; j++)
+            road[i][j] = ' ';
+    for (int j = 0; j < 15; j++) {
+        road[0][j] = '*';
+        road[4][j] = '*';
+        road[2][j] = '-';
+    }
 }
 
+void buildEastRoad(char road[5][15]) {
+    for (int i = 0; i < 5; i++)
+        for (int j = 0; j < 15; j++)
+            road[i][j] = ' ';
+    for (int j = 0; j < 15; j++) {
+        road[0][j] = '*';
+        road[4][j] = '*';
+        road[2][j] = '-';
+    }
+}
 
+void printVertical(const char westroad[5][15], const char eastroad[5][15]) {
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 15; j++)
+            cout << westroad[i][j];
+        cout << string(10, ' ');  // 10 spaces between roads
+        for (int j = 0; j < 15; j++)
+            cout << eastroad[i][j];
+        cout << endl;
+    }
+}
 
+void startWestCar(char westroad[5][15], vector<car>& westcar) {
+    car temp;
+    temp.row = 1;
+    temp.col = 0;
+    temp.position = "West";
+    westroad[temp.row][temp.col] = temp.represent;
+    westcar.push_back(temp);
+}
 
+int main() {
+    char westroad[5][15];
+    char eastroad[5][15];
 
-
-
-
-
-
-
-
-
-
-
-
-int main() {;
     vector<car> westcar;
-    char road[5][15];
-    westRoad(road);
-    startwestcar(road,car,westcar);
-  
+    WestJunction westJunc;
+    EastJunction eastJunc;
 
-  
+    buildWestRoad(westroad);
+    buildEastRoad(eastroad);
+
+    startWestCar(westroad, westcar);
+
+    printVertical(westroad, eastroad);
+
     return 0;
 }
-
